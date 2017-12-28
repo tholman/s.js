789174374f93108a59cca51f3bdf93c81d4e3ad7
Tim Holman <timothy.w.holman@gmail.com>
2017-12-28 20:28:05 +1000

    strokeS

--- s.js
+++ s.js
@@ -1,93 +1,42 @@
 (function() {
 
   // Check that functionality doesn't already exist in the browser.
-  if( CanvasRenderingContext2D.prototype.s && CanvasRenderingContext2D.prototype.fillS ) {
+  if( CanvasRenderingContext2D.prototype.strokeS && CanvasRenderingContext2D.prototype.fillS ) {
     return;
   }
 
-  CanvasRenderingContext2D.prototype.s = function(x, y, w, h) {
+  CanvasRenderingContext2D.prototype.strokeS = function(x, y, w, h) {
 
+    // These are the three secret sacred numbers of the beloved S
+    // ~ they are at the root of every argument in tech, hidden behind the scenes.
+    // ~ they are the one and only.
+    // ~ they are the secret to everything.
+    // ~ they are fibbonacchi's muse.
+    // ~ shh don't tell.
     var tipHeight = h * 0.15; // 15% * 2 (top and bottom)
     var lineHeight = h * 0.3; // 30% * 2 (main lines)
     var joinHeight = h * 0.1; // 10% (joining the center)
 
-    /**
-     *   /  \
-     *  
-     * 
-     *
-     *
-     **/
-    var localY = y;
-    this.moveTo(x, localY + tipHeight);
+    // The S is drawn in 2 lines, both originating from the center of the S
+    this.moveTo(x + w - w / 4, y + tipHeight + lineHeight + joinHeight / 2);
+    this.lineTo(x + w, y + tipHeight + lineHeight);
+    this.lineTo(x + w, y + tipHeight);
     this.lineTo(x + w / 2, y);
-    this.lineTo(x + w, localY + tipHeight);
-    
+    this.lineTo(x, y + tipHeight);
+    this.lineTo(x, y + tipHeight + lineHeight);
+    this.lineTo(x + w / 2, y + tipHeight + lineHeight + joinHeight)
+    this.lineTo(x + w / 2, y + tipHeight + lineHeight * 2 + joinHeight)
 
-    /**
-     *   /  \
-     *  | | |
-     * 
-     *
-     *
-     **/
-    localY = y + tipHeight;
-    this.moveTo(x, localY);
-    this.lineTo(x, localY + lineHeight);
-    this.moveTo(x + w / 2, localY);
-    this.lineTo(x + w / 2, localY + lineHeight);
-    this.moveTo(x + w, localY);
-    this.lineTo(x + w, localY + lineHeight);
+    this.moveTo(x + w / 4, y + tipHeight + lineHeight + joinHeight / 2);
+    this.lineTo(x, y + tipHeight + lineHeight + joinHeight)
+    this.lineTo(x, y + tipHeight + lineHeight * 2 + joinHeight)
+    this.lineTo(x + w / 2, y + tipHeight * 2 + lineHeight * 2 + joinHeight)
+    this.lineTo(x + w, y + tipHeight + lineHeight * 2 + joinHeight)
+    this.lineTo(x + w, y + tipHeight + lineHeight + joinHeight)
+    this.lineTo(x + w / 2, y + tipHeight + lineHeight)
+    this.lineTo(x + w / 2, y + tipHeight)
 
-
-    /**
-     *   /  \
-     *  | | |
-     *  join
-     *
-     *
-     **/
-    localY = y + tipHeight + lineHeight;
-    this.moveTo(x, localY);
-    this.lineTo(x + w / 2, localY + joinHeight);
-    this.moveTo(x + w / 2, localY);
-    this.lineTo(x + w, localY + joinHeight);
-    this.moveTo(x + w, localY);
-    this.lineTo(x + w - w / 4, localY + joinHeight / 2);
-    this.moveTo(x + w / 4, localY + joinHeight / 2);
-    this.lineTo(x, localY + joinHeight);
-
-    /**
-     *   /  \
-     *  | | |
-     *  join
-     *  | | |
-     *
-     **/
-
-    localY = y + tipHeight + lineHeight + joinHeight;
-    this.moveTo(x, localY);
-    this.lineTo(x, localY + lineHeight);
-    this.moveTo(x + w / 2, localY);
-    this.lineTo(x + w / 2, localY + lineHeight);
-    this.moveTo(x + w, localY);
-    this.lineTo(x + w, localY + lineHeight);
-
-    /**
-     *   /  \
-     *  | | |
-     *  join
-     *  | | |
-     *  \  /
-     **/
-
-    var localY = y + tipHeight + joinHeight + lineHeight * 2;
-    this.moveTo(x, localY);
-    this.lineTo(x + w / 2, localY + tipHeight);
-    this.moveTo(x + w, localY);
-    this.lineTo(x + w / 2, localY + tipHeight);
-
-
+    this.stroke();
   }
 
   CanvasRenderingContext2D.prototype.fillS = function(x, y, w, h) {
@@ -104,6 +53,5 @@ canvas.height = 500;
 var context = canvas.getContext('2d');
 
 context.lineWidth = 10;
-context.s(10, 10, 50, 300);
+context.strokeS(10, 10, 50, 300);
 
-context.stroke();
